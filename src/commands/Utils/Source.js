39a49d66f041c097cd26a48ff9d420b4c8e6ca80
Colderry <72723052+Colderry@users.noreply.github.com>
2021-05-01 02:26:53 +0600

    Source.js

---
+++ src/commands/Utils/Source.js
@@ -0,0 +1,23 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class Source extends Command {
+    constructor() {
+        super("source", {
+            aliases: ["source"],
+            channel: "guild",
+            category: "Utils",
+            description: "Link to the source of Aichan."
+        });
+    };
+
+    exec(message) {
+
+        return message.channel.send(
+            new MessageEmbed()
+                .setTitle("Source")
+                .setColor("#e46ea2")
+                .addField("Link", `[Click Here](${this.client.config.source})`)
+        );
+    };
+};
